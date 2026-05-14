347c2b80bcffe285fb28dfce52f625815c1b0264
Name <evn.strsdn@pm.me>
2026-05-13 20:39:08 -0700

    add soundcloud to links overlay

--- src/components/GlobalFx.tsx
+++ src/components/GlobalFx.tsx
@@ -40,6 +40,7 @@ const LINKS: { href: string; label: string }[] = [
   { href: 'https://www.instagram.com/_sys.ex__/', label: 'instagram.com/_sys.ex__' },
   { href: 'https://www.tiktok.com/@sys.ex', label: 'tiktok.com/@sys.ex' },
   { href: 'https://www.youtube.com/@_sysex', label: 'youtube.com/@_sysex' },
+  { href: 'https://soundcloud.com/sys_ex', label: 'soundcloud.com/sys_ex' },
   { href: 'https://3v4n.netlify.app', label: '3v4n.netlify.app' },
   { href: 'https://ko-fi.com/sysex', label: 'ko-fi.com/sysex' },
 ]
